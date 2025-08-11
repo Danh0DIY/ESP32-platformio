a7a89c75474a5725cb7ffa4e49a63d5c19daed80
Danh0DIY <dnn345169@gmail.com>
2025-08-11 21:40:26 +0700

    Create User_Setup.h

---
+++ src/User_Setup.h
@@ -0,0 +1,24 @@
+#define ST7735_DRIVER
+
+#define TFT_WIDTH  80
+#define TFT_HEIGHT 160
+
+#define ST7735_GREENTAB160x80
+
+#define TFT_MISO -1
+#define TFT_MOSI 23
+#define TFT_SCLK 18
+#define TFT_CS   22
+#define TFT_DC   21
+#define TFT_RST  19
+#define TFT_BL   -1
+
+#define LOAD_GLCD
+#define LOAD_FONT2
+#define LOAD_FONT4
+#define LOAD_FONT6
+#define LOAD_FONT7
+#define LOAD_FONT8
+#define LOAD_GFXFF
+
+#define SMOOTH_FONT
