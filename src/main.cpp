ba0cc05fc0ec3b9aef7cc51fc69cb661e5e2123b
Danh0DIY <dnn345169@gmail.com>
2025-08-11 21:30:17 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,15 @@
 
+// Code cơ bản nháy LED trên ESP32
+
+#define LED_PIN 2  // Chân GPIO có LED tích hợp
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT); // Cấu hình chân LED là OUTPUT
+}
+
+void loop() {
+  digitalWrite(LED_PIN, HIGH); // Bật LED
+  delay(500);                  // Chờ 500ms
+  digitalWrite(LED_PIN, LOW);  // Tắt LED
+  delay(500);                  // Chờ 500ms
+}
