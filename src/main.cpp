b2f95bfc5049fe43974b28ed0039e1eb56a5bcb8
Danh0DIY <dnn345169@gmail.com>
2025-08-11 21:54:48 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,11 @@ TFT_eSPI tft = TFT_eSPI();
 
 void setup() {
   tft.init();
-  tft.setRotation(1);
+  tft.setRotation(1); // Xoay ngang
   tft.fillScreen(TFT_BLACK);
+  
   tft.setTextColor(TFT_WHITE, TFT_BLACK);
-  tft.setTextSize(2);
-  tft.setCursor(10, 10);
-  tft.println("Hello ESP32!");
+  tft.drawString("Hello ESP32!", 10, 10, 2);
 }
 
 void loop() {
