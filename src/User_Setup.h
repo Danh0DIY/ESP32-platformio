e50e1a85709259db94066bf119cd040e1574d70f
Danh0DIY <dnn345169@gmail.com>
2025-08-11 22:25:19 +0700

    Update User_Setup.h

--- src/User_Setup.h
+++ src/User_Setup.h
@@ -1,17 +1,26 @@
+#define USER_SETUP_LOADED
+
+// Loại màn hình
 #define ST7735_DRIVER
+#define ST7735_GREENTAB160x80
+
+// Kích thước
 #define TFT_WIDTH  160
 #define TFT_HEIGHT 80
 
-// Chân kết nối
+// Chân SPI
 #define TFT_MOSI 18
 #define TFT_SCLK 5
 #define TFT_CS   22
 #define TFT_DC   21
 #define TFT_RST  19
-#define TFT_BL   -1 // bỏ qua, vì đã nối thẳng 3V3
+#define TFT_MISO -1 // không dùng
 
-// Loại LCD
-#define ST7735_GREENTAB160x80
-
-// Tốc độ SPI
-#define SPI_FREQUENCY  27000000
+// Màu chữ mặc định
+#define LOAD_GLCD
+#define LOAD_FONT2
+#define LOAD_FONT4
+#define LOAD_FONT6
+#define LOAD_FONT7
+#define LOAD_FONT8
+#define LOAD_GFXFF
