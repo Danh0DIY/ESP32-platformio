2dd9a156a3771430cb82cb2111d48532197a6909
Danh0DIY <dnn345169@gmail.com>
2025-08-11 22:40:19 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,11 @@ TFT_eSPI tft = TFT_eSPI();
 
 void setup() {
   tft.init();
-  tft.setRotation(1); // Xoay ngang
   tft.fillScreen(TFT_BLACK);
   tft.setTextColor(TFT_GREEN, TFT_BLACK);
-  tft.setTextSize(2);
+  tft.setTextSize(1);
   tft.setCursor(10, 30);
-  tft.println("Xin chao!");
+  tft.println("Xin chao bro, khoe khong! ");
 }
 
 void loop() {
