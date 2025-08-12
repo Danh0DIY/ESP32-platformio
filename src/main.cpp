39fd6b8aceaa7287ee19303a929a62b066af3960
Danh0DIY <dnn345169@gmail.com>
2025-08-12 07:22:15 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,39 @@
-#include <TFT_eSPI.h> // Thư viện TFT_eSPI
+#include <TFT_eSPI.h>
 #include <SPI.h>
 
-TFT_eSPI tft = TFT_eSPI();
-
-int x; // Tọa độ X của chữ
-String text = "Hello Nhung an khuu!"; // Nội dung chữ
-int textWidth;
+TFT_eSPI tft = TFT_eSPI(); 
 
 void setup() {
+  Serial.begin(115200);
   tft.init();
-  tft.setRotation(0); // Ngang
+  tft.setRotation(0); // Đổi từ 0-3 để test xoay
   tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_WHITE, TFT_BLACK);
-  tft.setTextSize(2);
 
-  // Tính độ rộng chữ
-  textWidth = tft.textWidth(text);
-  x = tft.width(); // Bắt đầu từ ngoài màn hình bên phải
+  // Lấy kích thước
+  int w = tft.width();
+  int h = tft.height();
+
+  // Vẽ khung viền
+  tft.drawRect(0, 0, w, h, TFT_WHITE);
+
+  // Vẽ các đường chéo để nhìn rõ góc
+  tft.drawLine(0, 0, w-1, h-1, TFT_RED);
+  tft.drawLine(w-1, 0, 0, h-1, TFT_RED);
+
+  // Ghi chữ ở các mép
+  tft.setTextColor(TFT_GREEN, TFT_BLACK);
+  tft.drawString("TOP", w/2 - 15, 2, 2);
+  tft.drawString("BOTTOM", w/2 - 30, h - 16, 2);
+  tft.drawString("LEFT", 2, h/2 - 8, 2);
+  tft.drawString("RIGHT", w - 35, h/2 - 8, 2);
+
+  // Ghi thông tin kích thước
+  tft.setTextColor(TFT_YELLOW, TFT_BLACK);
+  String info = "W=" + String(w) + " H=" + String(h);
+  tft.drawString(info, w/2 - 30, h/2 - 8, 2);
+
+  Serial.println(info);
 }
 
 void loop() {
-  tft.fillScreen(TFT_BLACK); // Xóa màn hình
-  tft.setCursor(x, (tft.height() - 16) / 2); // Căn giữa theo chiều cao
-  tft.print(text);
-
-  x--; // Di chuyển sang trái
-
-  // Nếu chữ đã chạy hết ra ngoài trái màn hình
-  if (x < -textWidth) {
-    x = tft.width(); // Reset lại bên phải
-  }
-
-  delay(20); // Điều chỉnh tốc độ chạy
 }
